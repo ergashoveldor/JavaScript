ef13a0df2eab0b4e5f7aa241d734ef69aa386866
ergashoveldor <eldor7147@gmail.com>
2022-05-22 07:52:26 +0500

    new post

--- JavaScript Fundamentals/1.Hello World/main.js
+++ JavaScript Fundamentals/1.Hello World/main.js
@@ -5,4 +5,15 @@
 //alert("I am JavaScript");
 console.log('Assalamu alaykum')
 
+// o'zgaruvchini nusxalash
+let name = 'John';
+let admin;
 
+admin = name;
+
+console.log(name);
+console.log(admin);
+
+// camelCase orqali o'zgaruvchilarni e'lon qilish
+const birthDay = '11.12.1990';
+const age = someCode(birthDay);
